6efd39a82c9c5affecd5c2ecdcf456465afc9fbe
Tony Yi <24402057+tony-ze-yi@users.noreply.github.com>
2022-12-08 00:08:21 -0500

    fix: time comparison

--- only-images.py
+++ only-images.py
@@ -1,42 +1,50 @@
+from time import sleep
+
 import discord
 import os
 import sys
 import datetime
 from discord.ext import tasks
 from discord import TextChannel
+import pytz
 
 STAR_THRESHOLD = 5
 DEL_THRESHOLD = 5
-IMAGES_CHANNEL_NAME = 'images'
+IMAGES_CHANNEL_NAME = "images"
 HI_CHAT_ID = 1032482927394693178
+tz = pytz.timezone('Canada/Eastern')
 
 try:
-    os.environ['DISCORD_TOKEN']
+    os.environ["DISCORD_TOKEN"]
 except KeyError:
-    print('no discord token idiot')
+    print("no discord token idiot")
     sys.exit(1)
 
-intents = discord.Intents(messages=True, reactions=True, guilds=True, message_content=True)
+intents = discord.Intents(
+    messages=True, reactions=True, guilds=True, message_content=True
+)
 client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
     purge_hi_chat_loop.start()
 
 
 @client.event
 async def on_message(message):
     if message.channel.id == HI_CHAT_ID:
-        print(f'deleting {message.id} in 15 minutes')
+        print(f"deleting {message.id} in 15 minutes")
         await message.delete(delay=900)
 
     if message.author == client.user:
         return
 
-    if (message.content != '' or len(message.attachments) == 0) and message.channel.name == IMAGES_CHANNEL_NAME:
-        print(f'deleting non only image in #{IMAGES_CHANNEL_NAME}')
+    if (
+        message.content != "" or len(message.attachments) == 0
+    ) and message.channel.name == IMAGES_CHANNEL_NAME:
+        print(f"deleting non only image in #{IMAGES_CHANNEL_NAME}")
         print(message.channel.name)
         await message.delete()
 
@@ -49,41 +57,56 @@ async def get_message_by_id(guild_id, channel_id, message_id):
 
 @client.event
 async def on_raw_reaction_add(raw_reaction_event):
-    message = await get_message_by_id(raw_reaction_event.guild_id, raw_reaction_event.channel_id,
-                                      raw_reaction_event.message_id)
+    message = await get_message_by_id(
+        raw_reaction_event.guild_id,
+        raw_reaction_event.channel_id,
+        raw_reaction_event.message_id,
+    )
     reactions = message.reactions
 
     print("reaction {}".format(raw_reaction_event.emoji.name))
 
-    if raw_reaction_event.emoji.name == '⭐':
+    if raw_reaction_event.emoji.name == "⭐":
         for react in reactions:
-            if react.emoji == '⭐' and react.count >= STAR_THRESHOLD:
-                print(f'starring {STAR_THRESHOLD}x⭐')
+            if react.emoji == "⭐" and react.count >= STAR_THRESHOLD:
+                print(f"starring {STAR_THRESHOLD}x⭐")
                 await message.pin()
 
-    elif raw_reaction_event.emoji.name == '👎' and message.channel.name == IMAGES_CHANNEL_NAME:
+    elif (
+        raw_reaction_event.emoji.name == "👎"
+        and message.channel.name == IMAGES_CHANNEL_NAME
+    ):
         for react in reactions:
-            if react.emoji == '👎' and react.count >= DEL_THRESHOLD:
-                print(f'deleting {DEL_THRESHOLD}x👎')
+            if react.emoji == "👎" and react.count >= DEL_THRESHOLD:
+                print(f"deleting {DEL_THRESHOLD}x👎")
                 await message.delete()
                 break
 
 
 async def purge_hi_chat():
-    print('purging hi chat')
+    print("purging hi chat")
     channel: TextChannel = client.get_channel(HI_CHAT_ID)
+    messages = [m async for m in channel.history(limit=100)]
 
-    try:
-        async for msg in channel.history(limit=500):
-            if msg.created_at < datetime.datetime.now() - datetime.timedelta(minutes=15):
-                print(f'deleting {msg.id} through 15 min loop')
-                await msg.delete()
-    except:
-        pass
+    for msg in messages:
+        print(
+            "msg created at: {}, now: {}, now - 15min: {}".format(
+                msg.created_at.astimezone(pytz.utc),
+                datetime.datetime.utcnow().astimezone(pytz.utc),
+                datetime.datetime.utcnow().astimezone(pytz.utc) - datetime.timedelta(minutes=15),
+            )
+        )
+        if msg.created_at.astimezone(pytz.utc) < datetime.datetime.utcnow().astimezone(pytz.utc) - datetime.timedelta(
+                minutes=15
+        ):
+            print(f"deleting {msg.id} through 15 min loop")
+            await msg.delete()
+            sleep(2)
 
 
 @tasks.loop(minutes=15)
 async def purge_hi_chat_loop():
     await purge_hi_chat()
 
-client.run(os.environ['DISCORD_TOKEN'])
+
+client.run(os.environ["DISCORD_TOKEN"])
