a566f7752facca9df7310dbf1b6ed9dd6b1286bf
Tony Yi <24402057+tony-ze-yi@users.noreply.github.com>
2023-04-02 13:09:22 -0400

    fix: shorten funny description

--- oi_discord_bot/cogs/Later.py
+++ oi_discord_bot/cogs/Later.py
@@ -93,8 +93,8 @@ class Later(commands.Cog):
 
     @app_commands.command(
         name="unlater",
-        description="Watch and pray that you may not enter into temptation. The spirit indeed is willing, "
-                    "but the flesh is weak. Matthew 26:41",
+        description="Watch and pray that you may not enter into temptation. The spirit is willing, but the flesh is "
+                    "weak.",
     )
     @app_commands.guilds(discord.Object(id=OI_GUILD_ID))
     async def unlater(self, interaction: Interaction):
